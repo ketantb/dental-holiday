a7154e601d4b335cab424285fc1faf6b51e56afc
Harshada0611 <harshadaa1997@gmail.com>
2023-08-09 15:37:33 +0530

    .

--- server/routes/account.js
+++ server/routes/account.js
@@ -16,42 +16,43 @@ router.post("/send-email", async (req, resp) => {
     const findAccount = await Account.findOne({ email: req.body.email });
     if (findAccount) {
       return resp.json({ success: false, message: "Email already registered" });
-    }
-    const otp = randomNumber;
-    const newData = await Account.create({ ...req.body, otp: otp });
+    } else {
+      const otp = randomNumber;
+      const newData = await Account.create({ ...req.body, otp: otp });
 
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.NODEMAILER_EMAIL,
-        pass: process.env.NODEMAILER_PASSWORD,
-      },
-    });
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.NODEMAILER_EMAIL,
+          pass: process.env.NODEMAILER_PASSWORD,
+        },
+      });
 
-    let mailOptions = {
-      from: process.env.NODEMAILER_EMAIL,
-      to: req.body.email,
-      subject: "OTP verificaion for Dental Mart",
-      text: `
+      let mailOptions = {
+        from: process.env.NODEMAILER_EMAIL,
+        to: req.body.email,
+        subject: "OTP verificaion for Dental Mart",
+        text: `
             Hello from Dental Holidays,
             A sign in attempt requires further verification because we did not recognise your device. 
             To complete the sign in, enter the verification code.
             Your verification code is : ${otp}
             Thanks.
             `,
-    };
+      };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-        return resp.status(500).json({ error: error });
-      } else {
-        console.log("Email sent: " + info.response);
-        return resp
-          .status(200)
-          .json({ success: true, message: info.response, newData: newData });
-      }
-    });
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          return resp.status(500).json({ error: error });
+        } else {
+          console.log("Email sent: " + info.response);
+          return resp
+            .status(200)
+            .json({ success: true, message: info.response, newData: newData });
+        }
+      });
+    }
   } catch (err) {
     console.log(err);
   }
@@ -59,10 +60,13 @@ router.post("/send-email", async (req, resp) => {
 
 //verify otp
 router.post("/verify-otp", async (req, resp) => {
+  console.log(req.body);
   try {
     const findAccount = await Account.findOne({ email: req.body.email });
     if (findAccount) {
-      if (findAccount.otp === req.body.otp) {
+      if (findAccount.otp === Number(req.body.otp)) {
+        findAccount.isVerified = true;
+        findAccount.save();
         resp.json({ success: true, message: "OTP verified successfully" });
       } else {
         resp.json({ success: false, message: "Invalid OTP" });
